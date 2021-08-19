03440fc0cf69c667941fc94a9de835fe347e975a
Tenysson Will de Lemos <tenyssonwill@gmail.com>
2021-08-19 23:23:50 +0000

    Banco de Dados

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, request, session, g, redirect,\
+	abort, render_template, flash
+
+# CONFIGURACAO
+DATABASE = 'blog.db'
+SECRET_KEY = 'pudim'
+
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+def connectar_bd():
+	return sqlite3.connect(app.config['DATABASE'])
 
 @app.route('/hello')
 def pagina_inicial():
-	return "Hello World"
+	return 'Hello World'
