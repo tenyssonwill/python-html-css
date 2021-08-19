93dd574397d68c9e6576369850f86001067bc3c3
Tenysson Will de Lemos <tenyssonwill@gmail.com>
2021-08-19 23:56:32 +0000

    Exibir entradas

--- app.py
+++ app.py
@@ -13,6 +13,18 @@ app.config.from_object(__name__)
 def connectar_bd():
 	return sqlite3.connect(app.config['DATABASE'])
 
+@app.before_request
+def antes_requisicao():
+	g.bd = connectar_bd()
+
+@app.teardown_request
+def depois_request(exc):
+	g.bd.close()
+
+@app.route('/')
+def exibir_entradas():
+	return render_template('exibir_entradas.html')
+
 @app.route('/hello')
 def pagina_inicial():
 	return 'Hello World'
