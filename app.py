c843ef7697c0ff5fee808df696d78f299f935aab
Tenysson Will de Lemos <tenyssonwill@gmail.com>
2021-08-27 00:06:51 +0000

    Final

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, request, session, g, redirect,\
+from flask import Flask, request, url_for, session, g, redirect,\
 	abort, render_template, flash
 
 # CONFIGURACAO
@@ -13,6 +13,7 @@ app.config.from_object(__name__)
 def connectar_bd():
 	return sqlite3.connect(app.config['DATABASE'])
 
+
 @app.before_request
 def antes_requisicao():
 	g.bd = connectar_bd()
@@ -22,8 +23,46 @@ def depois_request(exc):
 	g.bd.close()
 
 @app.route('/')
+@app.route('/entradas')
 def exibir_entradas():
-	return render_template('exibir_entradas.html')
+	sql ='SELECT titulo, texto FROM entradas ORDER BY id DESC'
+	cur = g.bd.execute(sql)
+	entradas = []
+	for titulo, texto in cur.fetchall():
+		entradas.append({'titulo': titulo, 'texto': texto})
+	return render_template('exibir_entradas.html', entradas=entradas)
+
+@app.route('/inserir', method = ["POST"])
+def inserir_entrada():
+	# sql = "INSERT INTO entradas(titulo, texto) VALUES ('Terceiro Post','Esse é post 3')"
+	# g.bd.execute(sql)
+	# g.bd.commit()
+	# return redirect(url_for('exibir_entradas'))
+	
+	if not session.get('logado'):
+		abort(401)
+	sql = 'INSERT INTO entradas(titulo, texto) VALUES (?,?)' 
+	g.bd.execute(sql, (request.form['campoTitulo'], request.form['campoTexto']))
+	g.bd.commit()
+	return redirect(url_for('exibir_entrada'))
+
+@app.route('/logout')
+def logout():
+	session.pop('logado', None)
+	return redirect(url_for('exibir_entradas'))
+
+@app.route('/login',methods=["GET","POST"])
+def login():
+	erro = None
+	if request.method == 'POST':
+		if request.form['campoUsuario'] !='admin' \
+			or request.form['campoSenha'] !='admin':
+			erro = 'Senha ou Usuário Inválidos'
+		else:
+			session['logado'] = True
+			return redirect(url_for('exibir_entradas'))
+
+	return render_template('login.html',erro=erro)
 
 @app.route('/hello')
 def pagina_inicial():
